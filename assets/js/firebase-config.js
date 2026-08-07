e0d3af73c62e9bc06cc474bf867b84c8f9e119a5
Lewis Diamond <lewisd049@icloud.com>
2026-08-07 21:25:28 +0100

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -2,5 +2,13 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
-const firebaseConfig = { apiKey:'YOUR_API_KEY', authDomain:'YOUR_PROJECT.firebaseapp.com', projectId:'YOUR_PROJECT_ID', storageBucket:'YOUR_PROJECT.appspot.com', messagingSenderId:'YOUR_MESSAGING_SENDER_ID', appId:'YOUR_APP_ID' };
+const firebaseConfig = {
+  apiKey: "AIzaSyBXstRTgwqYmV84ZJXEmN5d0zSGGITs-xI",
+  authDomain: "tdf-events.firebaseapp.com",
+  projectId: "tdf-events",
+  storageBucket: "tdf-events.firebasestorage.app",
+  messagingSenderId: "772375545394",
+  appId: "1:772375545394:web:e6d07bf5dcd37ebfbd1e2b",
+  measurementId: "G-E10LJY3M2Z"
+};
 const app=initializeApp(firebaseConfig); export const auth=getAuth(app); export const db=getFirestore(app);
